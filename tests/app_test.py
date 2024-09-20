63c1f19dbe8a4d6da10b547c826e41ab64654fe6
Marko <markogidej@yahoo.com>
2024-09-20 19:25:41 -0400

    new test

--- tests/app_test.py
+++ tests/app_test.py
@@ -10,11 +10,9 @@ class TestAPI(unittest.TestCase):
         fake = Faker()
         num1 = fake.random_number(digits=3)
         num2 = fake.random_number(digits=3)
+        sum = num1 + num2
         print(num1, num2)
-        payload = {'num1': num1,'num2': num2}
-        response = self.app.post('/sum', json=payload)
-        data = response.get_json()
-        self.assertEqual(data['result'], num1 + num2)
+        self.assertEqual(sum, num1 + num2)
 
 if __name__ == '__main__':
     unittest.main()
